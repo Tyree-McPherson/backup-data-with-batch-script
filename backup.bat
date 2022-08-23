97036a53864e94f7848d1ae02b45846912a257f9
Tyree McPherson <50240910+Tyree-McPherson@users.noreply.github.com>
2022-08-23 18:29:58 -0400

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -6,7 +6,7 @@ set "currentDate=%date:-=%"
 set "backupFolderName=BACKUP_FOLDER_NAME"
 set "excludedFolders=EXCLUDED_FOLDERS"
 
-@REM Get the last backup date from the text file.
+@REM Get the last backup date from the log file.
 For /F "UseBackQ Delims==" %%A In (%history%) Do set "lastDate=%%A"
 
 @REM Set the date to the last available date from the log file. If no date is present, use today's date.
